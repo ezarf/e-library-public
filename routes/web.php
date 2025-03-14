56717c01f5945d1baf06218ac91f3775211a8b36
ammarayyasy <ammarmojur@gmail.com>
2025-03-14 14:33:59 +0700

    1. install laravel, tailwind, tailwind ui

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('homepage');
+});
+Route::get('/hall', function () {
+    return view('hall');
+});
+Route::get('/about', function () {
+    return view('about');
+});
